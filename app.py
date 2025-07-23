7b70563a092af202a997ee1613d7d202caed3916
Stuart McFarlane <stuart@mcfarlane.nl>
2025-07-23 17:25:19 +0200

    returns imaage and filename

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+import base64
 
 app = FastAPI()
 
@@ -6,3 +9,17 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+file_path = '../faces/MDgyNTQ5LmpwZw.jpg'
+
+@app.post("/process")
+async def process():
+    print(">process")
+    with open(file_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    json_response = jsonable_encoder({
+        'filename': 'foreground.jpg',
+        'image': encoded_string
+    })
+    print("<process", json_response)
+    return JSONResponse(json_response)
